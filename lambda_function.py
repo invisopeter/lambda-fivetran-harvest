69b8c8532fbf485f0c2ff4c7c78b29671726545f
invisopeter <peter@inviso.dk>
2022-02-08 22:54:55 +0700

    Harvest projects

--- lambda_function.py
+++ lambda_function.py
@@ -1,17 +1,18 @@
 import json
+import requests
+import pprint
+
 def lambda_handler(request, context):
-    print(request);
+
     # Fetch records using api calls
-    (insertTransactions, deleteTransactions, newTransactionCursor) = api_response(request['state'], request['secrets'])
+    (insertTransactions, newTransactionCursor) = api_response(request['state'], request['secrets'])
     # Populate records in insert
     insert = {}
     insert['transactions'] = insertTransactions
-    delete = {}
-    delete['transactions'] = deleteTransactions
     state = {}
     state['transactionsCursor'] = newTransactionCursor
     transactionsSchema = {}
-    transactionsSchema['primary_key'] = ['order_id', 'date']
+    transactionsSchema['primary_key'] = ['id']
     schema = {}
     schema['transactions'] = transactionsSchema
     response = {}
@@ -19,8 +20,6 @@ def lambda_handler(request, context):
     response['state'] =  state
     # Add all the records to be inserted in response
     response['insert'] = insert
-    # Add all the records to be marked as deleted in response
-    response['delete'] = delete
     # Add schema defintion in response
     response['schema'] = schema
     # Add hasMore flag
@@ -28,20 +27,44 @@ def lambda_handler(request, context):
     return response
 
 def api_response(state, secrets):
+
     # your api call goes here
-    insertTransactions = [
-            {"date":'2017-12-31T05:12:05Z', "order_id":1001, "amount":'$1200', "discount":'$12'},
-            {"date":'2017-12-31T06:12:04Z', "order_id":1001, "amount":'$1200', "discount":'$12'},
-    ]
-    deleteTransactions = [
-            {"date":'2017-12-31T05:12:05Z', "order_id":1000, "amount":'$1200', "discount":'$12'},
-            {"date":'2017-12-31T06:12:04Z', "order_id":1000, "amount":'$1200', "discount":'$12'},
-    ]
-    return (insertTransactions, deleteTransactions, '2018-01-01T00:00:00Z')
+    endpoint = "https://invisodanmark.harvestapp.com/"
+    harvest_token ="639717.pt.YhyRXRe9At65SHz0YJNrIEV0yuSVUBfUPpmRGZLKR6XuoAyWFWEnuNlPniiqspwSCfX6EqfuNK80-bGTGinROA"
+    headers = {
+        'authorization': "Bearer " + harvest_token,
+        'content-type': "application/json",
+        'accept': "application/json",
+        'User-Agent': 'Harvest API Example',
+        'Harvest-Account-ID': '586549'
+    }
+
+    payload={}
+
+    url = "https://api.harvestapp.com/v2/projects?updated_since="+request['state']['transactionsCursor']
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    data  = response.json()
+    data_content = data["projects"]
+
+    maxs = max(data_content, key=lambda ev: ev['updated_at'])["updated_at"]
+
+    data_content = [dict(
+        id=k1["id"],
+        name=k1["name"],
+        updated_at=k1["updated_at"],
+    ) for k1 in data_content]
+
+    #print(json.dumps(data_content, indent=4, sort_keys=True))
+
+
+    insertTransactions = data_content
+
+
+    return (insertTransactions, '2018-01-01T00:00:00Z')
 
 #request = {}
-#request['state'] = "state"
-#request['secrets'] = "secret"
+#request['state'] = {'transactionsCursor': '2018-01-01T00:00:00Z'}
+#request['secrets'] = 'secret'
 
 #ref = lambda_handler(request, "context")
-#print(ref)
