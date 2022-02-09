ead633acd0ab4e4c2a702aab6c5d504b3a144352
invisopeter <peter@inviso.dk>
2022-02-09 11:31:02 +0700

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ import pprint
 
 def lambda_handler(request, context):
 
+    print(json.dumps(request, indent=4, sort_keys=True))
+
     # Fetch records using api calls
     (insertTransactions, newTransactionCursor) = api_response(request['state'], request['secrets'])
     # Populate records in insert
