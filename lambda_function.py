e6a8b940cd20413cc8b256ab2e14c2ccd8d0bd83
invisopeter <peter@inviso.dk>
2022-02-11 20:11:29 +0100

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -7,9 +7,36 @@ import configparser
 config = configparser.ConfigParser()
 config.read('config.ini')
 
+targets = {
+  "projects": {
+    "path":"projects",
+    "primary_key":['id'],
+    "columns":['id','name','updated_at','created_at'],
+    "cursor":"2014-01-01T00:00:00Z", #default
+    "page": 1, #default
+    "insert": {}
+  }
+}
+
 def lambda_handler(request, context):
 
-    print(request)
+    state = request['state']
+
+    # Update with state
+
+    # z = dict_merge(targets,state)
+
+    for target in targets:
+        targets[target]["cursor"] = state[target]["cursor"]
+        targets[target]["page"] = state[target]["page"]
+        #print(dump(targets[target]))
+        #(state, insertTransactions, hasMore) = get_updates(targets[target], state)
+
+        #prep respnse
+
+    print(dump(targets))
+    exit()
+
 
     # Fetch records using api calls
     (insertTransactions, newState, hasMore) = api_response(request['state'], request['secrets'])
@@ -39,6 +66,11 @@ def lambda_handler(request, context):
     #print(json.dumps(response, indent=4, sort_keys=True))
 
     return response
+
+def get_updates(target, state):
+
+
+    return ({},{},{})
 
 def api_response(state, secrets):
 
@@ -99,9 +131,29 @@ def api_response(state, secrets):
 
     return (insertTransactions, state, hasMore)
 
+def dump(response):
+    return json.dumps(response, indent=4, sort_keys=True)
+
+
+def dict_merge(dct, merge_dct):
+    """ Recursive dict merge. Inspired by :meth:``dict.update()``, instead of
+    updating only top-level keys, dict_merge recurses down into dicts nested
+    to an arbitrary depth, updating keys. The ``merge_dct`` is merged into
+    ``dct``.
+    :param dct: dict onto which the merge is executed
+    :param merge_dct: dct merged into dct
+    :return: None
+    """
+    for k, v in merge_dct.iteritems():
+        if (k in dct and isinstance(dct[k], dict)
+                and isinstance(merge_dct[k], collections.Mapping)):
+            dict_merge(dct[k], merge_dct[k])
+        else:
+            dct[k] = merge_dct[k]
+
 if os.environ.get("AWS_EXECUTION_ENV") is None:
     request = {}
-    request['state'] = {'cursor': '2022-02-08T20:56:27Z'}
+    request['state'] = {"projects": {'cursor': '2022-02-08T20:56:27Z', 'page' :4}}
     #request['state'] = {}
-    request['secrets'] = 'secret'
+    #request['secrets'] = 'secret'
     ref = lambda_handler(request, "context")
